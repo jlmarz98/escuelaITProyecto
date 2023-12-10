8b8ef9ba65dea557af30523091ccc9ea5f443c51
jlmarz98 <34523567+jlmarz98@users.noreply.github.com>
2023-12-10 15:54:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	
-	<h1>Hola Bienvenidos</h1>
 
-	<h2>todo para el hogar</h2>
-<h3>TODO DESDE AQUI</h3>
-<h4>ayer fue 31</h4>
-<h5>debo aprender mas rapido</h5>
-</body>
-</html>
